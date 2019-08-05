fbc187c14338eab16583b5566794a76efc89f7c8
frankfang <frankfang1990@gmail.com>
2019-08-05 13:56:55 +0800

    支持 title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,10 +8,10 @@ import Date2 from '../lib/Date2';
 import queryString from 'query-string';
 
 
-const query = queryString.parse(window.location.search);
 const App: React.FC = () => {
+  const query = queryString.parse(window.location.search);
   const [events, setEvents] = useState<CalendarEvent[]>([]);
-  const {selected} = query;
+  const {selected, title} = query;
   const [date, setDate] = useState(selected ? new Date(selected.toString()) : new Date());
   const selectedEvents = () => {
     return events.filter((e) =>
@@ -21,6 +21,9 @@ const App: React.FC = () => {
   };
   useEffect(() => {
     if (!query.eventsUrl) {return;}
+    if (query.title) {
+      document.title = query.title as string;
+    }
     axios.get(query.eventsUrl.toString()).then((response) => {
       const data = y.safeLoad<CalendarEvent[]>(response.data);
       setEvents(data);
@@ -28,7 +31,7 @@ const App: React.FC = () => {
   }, []);
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.h1}>课程表 - 建议收藏</h1>
+      <h1 className={styles.h1}>{title || '课程表'} - 建议收藏</h1>
       <p className={styles.p}>小圆点表示有课程</p>
       <Calendar events={events} value={date} onChange={setDate}/>
       <EventList title={`${new Date2(date).toString('M月d日')}的安排`} events={selectedEvents()}/>

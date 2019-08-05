403d286a49e984a2c7f01787c0947faa6fcae157
frankfang <frankfang1990@gmail.com>
2019-08-05 13:58:28 +0800

    update title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -31,8 +31,8 @@ const App: React.FC = () => {
   }, []);
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.h1}>{title || '课程表'} - 建议收藏</h1>
-      <p className={styles.p}>小圆点表示有课程</p>
+      <h1 className={styles.h1}>{title || '课程表'}</h1>
+      <p className={styles.p}>小圆点表示有课程，建议添加本页到收藏</p>
       <Calendar events={events} value={date} onChange={setDate}/>
       <EventList title={`${new Date2(date).toString('M月d日')}的安排`} events={selectedEvents()}/>
       <footer className={styles.footer}>本页面使用 React 构建，源代码托管于 &nbsp;

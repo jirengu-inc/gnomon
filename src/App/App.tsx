301b851851e237dd4e87248a4409509838ff300f
frankfang <frankfang1990@gmail.com>
2019-08-05 14:06:00 +0800

    支持 title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,11 +7,16 @@ import EventList from '../EventList/EventList';
 import Date2 from '../lib/Date2';
 import queryString from 'query-string';
 
+interface OpenEvents {
+  title: string;
+  events: CalendarEvent[]
+}
 
 const App: React.FC = () => {
   const query = queryString.parse(window.location.search);
   const [events, setEvents] = useState<CalendarEvent[]>([]);
-  const {selected, title} = query;
+  const {selected} = query;
+  const [title, setTitle] = useState('课程表');
   const [date, setDate] = useState(selected ? new Date(selected.toString()) : new Date());
   const selectedEvents = () => {
     return events.filter((e) =>
@@ -21,17 +26,18 @@ const App: React.FC = () => {
   };
   useEffect(() => {
     if (!query.eventsUrl) {return;}
-    if (query.title) {
-      document.title = query.title as string;
-    }
     axios.get(query.eventsUrl.toString()).then((response) => {
-      const data = y.safeLoad<CalendarEvent[]>(response.data);
-      setEvents(data);
+      const data = y.safeLoad<OpenEvents>(response.data);
+      setEvents(data.events);
+      if (data.title) {setTitle(data.title);}
     });
   }, []);
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.h1}>{title || '课程表'}</h1>
+      <h1 className={styles.h1}>{title}</h1>
       <p className={styles.p}>小圆点表示有课程，建议添加本页到收藏</p>
       <Calendar events={events} value={date} onChange={setDate}/>
       <EventList title={`${new Date2(date).toString('M月d日')}的安排`} events={selectedEvents()}/>
